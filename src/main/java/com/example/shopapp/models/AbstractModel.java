a18ba76e226b704f51f343e4af5d71cc60795366
NHTD <nhtd7502@gmail.com>
2024-05-23 15:54:05 +0700

    Create crud of Category models

--- src/main/java/com/example/shopapp/models/AbstractModel.java
+++ src/main/java/com/example/shopapp/models/AbstractModel.java
@@ -1,19 +1,37 @@
 package com.example.shopapp.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.Hibernate;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @MappedSuperclass
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class AbstractModel implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        AbstractModel that = (AbstractModel) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/main/java/com/example/shopapp/repositories/OrderDetailRepository.java
+++ src/main/java/com/example/shopapp/repositories/OrderDetailRepository.java
@@ -1,8 +1,9 @@
 package com.example.shopapp.repositories;
 
+import com.example.shopapp.models.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetailRepository, Long> {
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
 }
